e33f13ab0bdbde73e4fe38964497960476731830
Gil Emmanuel Bancud <gembancud@gmail.com>
2020-06-21 16:25:16 +0800

    Connected Authentication to Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,90 @@
-import React from 'react'
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav'
-import Container from 'react-bootstrap/Container'
-import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
-import { AwesomeButton } from 'react-awesome-button';
-import 'react-awesome-button/dist/themes/theme-blue.css';
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import { AwesomeButton } from "react-awesome-button";
+import "react-awesome-button/dist/themes/theme-blue.css";
+import { connect } from "react-redux";
+import { signIn, signOut } from "../../actions";
 
+class Header extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "668642444476-5f3s8ja2jk0fj49kbgeo89481sjuu97q.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.onAuthChange(this.auth.isSignedIn.get());
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
+    });
+  }
 
-const Header = () => {
-  return (
-    <Navbar bg="primary" variant="dark" sticky="top">
-      <Container>
-        <Navbar.Brand as={Link} to="/">
-          <AwesomeButton type="primary" size="large">Teleskwela</AwesomeButton>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link as={Link} to="/learn">
-              <AwesomeButton type="secondary">Matuto</AwesomeButton>
-            </Nav.Link>
-          </Nav>
-          <Nav className="justify-content-end">
-            <Button variant="success">Sumali</Button>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+  onAuthChange = (isSignedIn) => {
+    if (isSignedIn) this.props.signIn(this.auth.currentUser.get().getId());
+    else this.props.signOut();
+  };
+
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
+  renderAuthenticationButton = () => {
+    if (!this.props.auth.isSignedIn) {
+      return (
+        <Button variant="success" onClick={this.onSignInClick}>
+          Log In
+        </Button>
+      );
+    } else {
+      return (
+        <Button variant="success" onClick={this.onSignOutClick}>
+          Sign Out
+        </Button>
+      );
+    }
+  };
+
+  render() {
+    return (
+      <Navbar bg="primary" variant="dark" sticky="top">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            <AwesomeButton type="primary" size="large">
+              Teleskwela
+            </AwesomeButton>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link as={Link} to="/learn">
+                <AwesomeButton type="secondary">Matuto</AwesomeButton>
+              </Nav.Link>
+            </Nav>
+            <Nav className="justify-content-end">
+              {this.renderAuthenticationButton()}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
 }
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    auth: state.auth,
+  };
+};
+
+export default connect(mapStateToProps, { signIn, signOut })(Header);
